096de8914adfea84058831b2f8df4f2ba2fa1dcf
walebign3 <walebign3@gmail.com>
2022-07-01 16:57:03 +0300

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    n = len(sys.argv) - 1
+
+    if n == 0:
+        print("{} arguments.".format(n))
+    elif n == 1:
+        print("{} argument:".format(n))
+    else:
+        print("{} arguments:".format(n))
+
+    if n >= 1:
+        n = 0
+        for arg in sys.argv:
+            if n != 0:
+                print("{}: {}".format(n, arg))
+            n = n + 1
