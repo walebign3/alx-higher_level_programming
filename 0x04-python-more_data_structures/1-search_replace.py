7e7e6e05d72b8eca621b311faaad0b516b2d2ffe
walebign3 <walebign3@gmail.com>
2022-07-11 02:54:42 +0300

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python
 def search_replace(my_list, search, replace):
-    n = 0
-    mynew_list = my_list.copy()
-    for i in range(len(mynew_list)):
-        c = mynew_list.count(mynew_list[i])
-        if c == search:
-            n = mynew_list[i]
-            for j in range(len(mynew_list)):
-                if c == mynew_list[j]:
-                    mynew_list[j] = replace
-    return (mynew_list)
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return (new_list)
 
