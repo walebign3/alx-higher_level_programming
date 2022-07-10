9c420f0b0774d6dc83dfb611553d6ed81e92e2b4
walebign3 <walebign3@gmail.com>
2022-07-10 11:37:08 +0300

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix2 = [[y**2 for y in x] for x in matrix]
-    return (matrix2)
+    matrix2 = matrix.copy()
+    return ([[y**2 for y in x] for x in matrix2])
