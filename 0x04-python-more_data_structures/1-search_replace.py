163851f68ca7dd0577242855b89863218196a03c
walebign3 <walebign3@gmail.com>
2022-07-11 02:46:30 +0300

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
 def search_replace(my_list, search, replace):
+    n = 0
     mynew_list = my_list.copy()
     for i in range(len(mynew_list)):
-        if mynew_list[i] == 2:
-            mynew_list[i] = 89
+        c = mynew_list.count(mynew_list[i])
+        if c == search:
+            n = mynew_list[i]
+            for j in range(len(mynew_list)):
+                if c == mynew_list[j]:
+                    mynew_list[j] = replace
     return (mynew_list)
 
