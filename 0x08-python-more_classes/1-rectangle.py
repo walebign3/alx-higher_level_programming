80a40a81e2170a4f1fc5c357150135eb19e98424
walebign3 <walebign3@gmail.com>
2022-07-25 17:24:20 +0300

    1-rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,14 +11,14 @@ class Rectangle:
     def set_width (self, width):
         """ Class Rectangle """
         if (type(width) != int):
-            raise ValueError('width must be an integer')
+            raise TypeError('width must be an integer')
         if (width < 0):
             raise ValueError('width must be >= 0')
         self._width = width
     def set_height (self, height):
         """ Class Rectangle """
         if (type(height) != int):
-            raise ValueError('height must be an integer')
+            raise TypeError('height must be an integer')
         if (height < 0):
             raise ValueError('height must be >= 0')
         self._height = height
