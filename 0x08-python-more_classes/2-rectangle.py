cfae77284956c55dafa682c8431b1afbf3896961
walebign3 <walebign3@gmail.com>
2022-07-26 05:26:33 +0300

    2-main.py

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" Class Rectangle """
+
+
+class Rectangle():
+    """ Class Rectangle """
+    def __init__(self, width=0, height=0):
+        """ Class Rectangle """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ Class Rectangle """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Class Rectangle """
+        if (type(value) != int):
+            raise TypeError('width must be an integer')
+        if (value < 0):
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Class Rectangle """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Class Rectangle """
+        if (type(value) != int):
+            raise TypeError('height must be an integer')
+        if (value < 0):
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
