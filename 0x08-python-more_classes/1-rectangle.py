cb2a0c81bae894edd82a3504ebd3320f7561de74
walebign3 <walebign3@gmail.com>
2022-07-26 04:28:20 +0300

    1-rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,10 +5,21 @@
 class Rectangle:
     """ Class Rectangle """
     def __init__(self, width=0, height=0):
-        self.set_width(width)
-        self.set_height(height)
+        self._width = width
+        self._height = height
 
-    def set_width(self, width):
+    @property
+    def width(self):
+        """ Class Rectangle """
+        return self._width
+
+    @property
+    def height(self):
+        """ Class Rectangle """
+        return self._height
+
+    @width.setter
+    def width(self, width):
         """ Class Rectangle """
         if (type(width) != int):
             raise TypeError('width must be an integer')
@@ -16,7 +27,8 @@ class Rectangle:
             raise ValueError('width must be >= 0')
         self._width = width
 
-    def set_height(self, height):
+    @height.setter
+    def height(self, height):
         """ Class Rectangle """
         if (type(height) != int):
             raise TypeError('height must be an integer')
@@ -24,13 +36,6 @@ class Rectangle:
             raise ValueError('height must be >= 0')
         self._height = height
 
-    def get_width(self):
-        """ Class Rectangle """
-        return self._width
-
-    def get_height(self):
-        """ Class Rectangle """
-        return self._height
-
-    width = property(fget = get_width, fset = set_width)
+"""    width = property(fget = get_width, fset = set_width)
     height = property(fget = get_height, fset = set_height)
+"""
