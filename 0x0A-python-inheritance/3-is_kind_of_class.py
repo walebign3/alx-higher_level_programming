4f760faca0b660ca7903ce6198bc44d7119e6752
walebign3 <walebign3@gmail.com>
2022-08-01 23:06:52 +0300

    3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Returns True if the object is an instance of
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Returns True if the object is an instance of,
+    """
+    return isinstance(obj, a_class)
