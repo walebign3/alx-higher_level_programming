33df001ece205a8ae63a23c5787041e582604b73
walebign3 <walebign3@gmail.com>
2022-08-01 22:17:15 +0300

    tests

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" A program that creates a class that inherits of the class List """
+
+
+class MyList(list):
+    """ A program that creates a class that inherits of the class List """
+
+    def print_sorted(self):
+        print(sorted(self))
