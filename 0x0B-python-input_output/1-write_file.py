558a412131bea9d0d538f791ff952669c24afed4
walebign3 <walebign3@gmail.com>
2022-08-02 20:36:34 +0300

    0-main.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+""" A function that reads a text file (UTF8) and prints it to stdout: """
+
+
 def write_file(filename="", text=""):
+    """ A function that reads from a file """
+
     with open(filename, 'w', encoding="utf-8") as f:
         return f.write(text)
