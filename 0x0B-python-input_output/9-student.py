d016c1e300d392700ece6c7e5d8c3e061f2d32a0
walebign3 <walebign3@gmail.com>
2022-08-03 17:26:01 +0300

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+a class Student that defines a student by:
+
+Public instance attributes:
+first_name
+last_name
+age
+"""
+class Student:
+    """ a class Student that defines a student by: """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ Method that returns directory description """
+        return self.__dict__.copy()
+
