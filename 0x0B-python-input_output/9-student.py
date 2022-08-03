3517753a584aa467f26f0d818cc596f3ced61139
walebign3 <walebign3@gmail.com>
2022-08-03 17:27:10 +0300

    9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,6 +9,7 @@ age
 """
 class Student:
     """ a class Student that defines a student by: """
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
