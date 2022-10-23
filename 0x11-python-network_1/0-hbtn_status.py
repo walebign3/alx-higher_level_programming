b1abc731bf9d832bba1d50c7f83436823514a965
walebign3 <walebign3@gmail.com>
2022-10-23 21:09:18 +0300

    fetch

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import urllib.request
+"""script that fetches https://alx-intranet.hbtn.io/status"""
+
+url = "https://alx-intranet.hbtn.io/status"
+if __name__ == "__main__":
+    with urllib.request.urlopen(url) as response:
+        data = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode("utf-8")))
