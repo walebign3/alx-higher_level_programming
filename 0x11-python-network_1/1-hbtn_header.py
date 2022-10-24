1ffae41e3591c1f1881bf19d8e694f34009185b4
walebign3 <walebign3@gmail.com>
2022-10-24 04:02:10 +0300

    x-re

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,8 +4,7 @@ import urllib.request
 import sys
 
 
-url = sys.argv[1]
 if __name__ == "__main__":
-    with urllib.request.urlopen(url) as response:
+    with urllib.request.urlopen(sys.argv[1]) as response:
         data = response.info()
         print(data["X-Request-Id"])
