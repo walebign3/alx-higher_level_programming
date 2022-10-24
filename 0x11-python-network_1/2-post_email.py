f1260e608e0a838a20b38fe873b53190c8ae9406
walebign3 <walebign3@gmail.com>
2022-10-24 04:46:05 +0300

    post

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""script that send POST"""
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == "__main__":
+    params = {"email": sys.argv[2]}
+    query_string = urllib.parse.urlencode(params)
+    data = query_string.encode("ascii")
+    with urllib.request.urlopen(sys.argv[1], data) as response:
+        print(response.read())
