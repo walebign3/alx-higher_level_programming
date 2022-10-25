225fc517915c37c3caf9d5aa051438177d9230b9
walebign3 <walebign3@gmail.com>
2022-10-25 12:56:54 +0300

    re

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,6 +6,9 @@ import sys
 
 if __name__ == "__main__":
     r = requests.get(sys.argv[1])
-    if r.status_code > 400:
-        print('Error code:', r.status_code)
-    print(r.content.decode("utf-8"))
+    try:
+        if r.status_code > 400:
+            print('Error code:', r.status_code)
+        else: print(r.content.decode("utf-8"))
+    except KeyError:
+        pass
