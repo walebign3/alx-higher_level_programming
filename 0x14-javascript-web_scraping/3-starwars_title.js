2aaca78eb183b8ac5e86a43683f8ad0989ebd0e1
walebign3 <walebign3@gmail.com>
2022-11-15 22:24:56 +0300

    req

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,5 +4,9 @@ const request = require('request');
 
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
-  console.log(JSON.parse(body).title);
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
 });
