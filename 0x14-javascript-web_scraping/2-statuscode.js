a6b0e52e377d109cf1a085f2de0ecc684a36d206
walebign3 <walebign3@gmail.com>
2022-11-15 21:45:36 +0300

    req

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,9 @@
 
 const request = require('request');
 
-request.get(process.argv[2]).on('response', function(response) {
+request
+  .get(process.argv[2])
+  .on('response', function(response) {
     console.log('code: ' + response.statusCode)
   })
 
