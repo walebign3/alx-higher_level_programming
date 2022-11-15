60afd15bc5e6a4f11d44f27324c53a9ac683e4dd
walebign3 <walebign3@gmail.com>
2022-11-15 20:31:23 +0300

    jvs

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
+
 const fs = require('fs')
 let arguments = process.argv
+
 fs.readFile(arguments[2], 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   }
-  console.log(data);
+  else {
+    console.log(data);
+  }
 })
